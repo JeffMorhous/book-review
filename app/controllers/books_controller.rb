e160393f5def271f0fd029d10e4797503ce262ab
= <=>
2020-07-22 14:24:43 -0400

    Add categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,28 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+     @books = Book.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def new
     @book = current_user.books.build
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def show
   end
 
   def edit
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -30,18 +38,18 @@ class BooksController < ApplicationController
 
   def create
     @book = current_user.books.build(book_params)
-
+    @book.category_id = params[:category_id]
     if @book.save
       redirect_to root_path
     else
-      redirect_to 'new'
+      redirect_to "new"
     end
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :description, :author)
+    params.require(:book).permit(:title, :description, :author, :category_id)
   end
 
   def find_book
